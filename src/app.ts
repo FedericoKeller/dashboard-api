6e77aa1eb714d85001a49d90af77ce10eaa36919
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-11-21 09:56:21 -0300

    feature(global): establishing global structure

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,47 @@
+/**
+ * Required External Modules
+ */
+
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 import express, { Request, Response, NextFunction } from "express";
 import { connect } from "mongoose";
-
-import DATABASE_CONNECTION from "./utils/database";
-
 import { authRoutes } from "./routes/auth.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+if (!process.env.PORT && !process.env.DATABASE_CONNECTION) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const DATABASE_CONNECTION: string = process.env.DATABASE_CONNECTION  as string;
 
 const app = express();
 
+/**
+ *  App Configuration
+ */
+
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-})
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 const connection = async () => {
   await connect(DATABASE_CONNECTION);
-  app.listen(8080);
+  app.listen(PORT);
   console.log("Client connected");
 };
 
