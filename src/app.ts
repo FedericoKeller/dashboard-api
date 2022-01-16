4336a491678049a2ae236528b611d952bfe74129
Kelprim <43957146+Kelprim@users.noreply.github.com>
2022-01-16 11:20:15 -0300

    Heroku config

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,9 @@ dotenv.config();
  * App Variables
  */
 
-if (!process.env.PORT && !process.env.DATABASE_CONNECTION) {
-  process.exit(1);
-}
+// if (!process.env.PORT && !process.env.DATABASE_CONNECTION) {
+//   process.exit(1);
+// }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const DATABASE_CONNECTION: string = process.env.DATABASE_CONNECTION  as string;
--- src/services/email-templates.ts
+++ src/services/email-templates.ts
@@ -1,6 +1,8 @@
 import { BaseUser } from "../models/user/user.interface";
 import mjml2html from "mjml";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 export module EmailTemplatesModule {
   export function getActivateAccountTemplate(
     user: BaseUser,
@@ -22,7 +24,7 @@ export module EmailTemplatesModule {
     <mj-column vertical-align="middle" width="100%">
       <mj-text align="left" color="#ffffff" font-size="22px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px"><span style="color:hsl(236, 100%, 87%)">Dear ${user.email}</span><br /><br /> Welcome to DashSmart.</mj-text>
       <mj-text align="left" color="#ffffff" font-size="15px" line-height=2 font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px">We&apos;re really excited you&apos;ve decided to give us a try. In case you have any questions, feel free to reach out to us at ${process.env.MAIL_USERNAME}. In order to proceed, we need you to activate your account. Hope to see you soon!</mj-text>
-      <mj-button align="left" font-size="22px"background-color="hsl(246, 80%, 60%)" border-radius="10px" color="#fff" font-family="open Sans Helvetica, Arial, sans-serif" href="http://localhost:4200/register/${token}">Activate Account</mj-button>
+      <mj-button align="left" font-size="22px"background-color="hsl(246, 80%, 60%)" border-radius="10px" color="#fff" font-family="open Sans Helvetica, Arial, sans-serif" href=${process.env.NODE_ENV === 'production' ? `${process.env.PRODUCTION_URL}register/${token}` : `${process.env.DEV_URL}register/${token}`}>Activate Account</mj-button>
       <mj-text align="left" color="#ffffff" font-size="15px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px" line-height=2>Thanks, <br /> The DashSmart Team</mj-text>
     </mj-column>
   </mj-section>
@@ -47,7 +49,7 @@ export module EmailTemplatesModule {
           <mj-text font-size="20px" color="#00A390" font-family="helvetica" line-height=2>
           Your DashSmart password can be reset by clicking the button below. If you did not request a new password, please ignore this email.
           </mj-text>
-                <mj-button align="center" padding-bottom="20px" padding-top="20px" font-size="22px"background-color="#00A390" border-radius="10px" color="#fff" font-family="open Sans Helvetica, Arial, sans-serif" href="http://localhost:4200/reset/${token}">Reset Password</mj-button>
+                <mj-button align="center" padding-bottom="20px" padding-top="20px" font-size="22px"background-color="#00A390" border-radius="10px" color="#fff" font-family="open Sans Helvetica, Arial, sans-serif" href=${process.env.NODE_ENV === 'production' ? `${process.env.PRODUCTION_URL}reset/${token}` : `${process.env.DEV_URL}reset/${token}`}>Reset Password</mj-button>
   
                   <mj-text font-size="20px" color="#00A390" font-family="helvetica">The DashSmart Team</mj-text>
         </mj-column>
